a49a7bd8fa55c6d52070e72ae4015dd3f9a92ae4
BoogiemanNSK <calah47@yandex.ru>
2020-04-24 21:35:54 +0300

    More music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioClip Main, Ending;
+    public AudioSource Audio;
+    
+    private void Start() {
+        Audio.loop = true;
+        Audio.clip = Main;
+        Audio.Play();
+        
+        EventManager.StartListening(Constants.WIN_EVENT_NAME, SwitchTrack);
+        EventManager.StartListening(Constants.LOSE_EVENT_NAME, SwitchTrack);
+    }
+
+    private void SwitchTrack() {
+        Audio.clip = Ending;
+        Audio.Play();
+    }
+
+}
